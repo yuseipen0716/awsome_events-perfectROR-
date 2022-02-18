c3f4a66d2db3d7d7eee7dcf89fffef546ec215f1
yuseipen0716 <yuseipen1@gmail.com>
2022-02-18 14:46:57 +0900

    fix method_name

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
         nickname = auth_hash[:info][:nickname]
         image_url = auth_hash[:info][:image]
 
-        User.finde_or_create_by!(provider: provider, uid: uid) do |user|
+        User.find_or_create_by!(provider: provider, uid: uid) do |user|
             user.name = nickname
             user.image_url = image_url
         end
