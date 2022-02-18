b3b80ed4e0d3b398aa923f1f18a93b8ed1c859ac
yuseipen0716 <yuseipen1@gmail.com>
2022-02-18 14:11:07 +0900

    create find_or_from_auth_hash method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    def self.find_or_create_from_auth_hash!(auth_hash)
+        provider = auth_hash[:provider]
+        uid = auth_hash[:uid]
+        nickname = auth_hash[:info][:nickname]
+        image_url = auth_hash[:info][:image]
+    end
 end
